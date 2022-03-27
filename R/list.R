f71a76bee26e3bf131aba96044dce02a1e758c48
dciwalsh <dciwalsh@gmail.com>
2022-03-27 16:02:17 +1300

    2022-03-27

--- R/list.R
+++ R/list.R
@@ -1,5 +1,20 @@
 ## --- List functions
 
+#' Create vector of mode list of length n.
+#'
+#' @param n Length of vector.
+#'
+#' @return Vector of mode list of length n.
+#'
+#' @keywords vector list
+#'
+#' @examples
+#'
+#' ## Simulate data and get initial model parameter estimates
+#' A <- listvector (n=3)
+#'
+#' @export
+#'
 listvector <- function (n=1) {
   return (vector(mode="list", length=n))
 }
